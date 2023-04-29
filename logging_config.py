3df082ff679f401055184622c8ad26d7bef7117d
vlad andrei <vvladalistar@gmail.com>
2023-04-29 12:18:59 +0300

    change logging debug to logging error

--- logging_config.py
+++ logging_config.py
@@ -8,7 +8,7 @@ from utils import BASE_DIR
 
 log_file_path = os.path.join(BASE_DIR, "log.log")
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.ERROR,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
         logging.FileHandler(log_file_path),
